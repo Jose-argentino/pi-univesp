41d691990c8a5a2b558a772ff4a199b6cdf3a0e2
larissa-nemes <lari.nemes@gmail.com>
2025-05-07 00:15:27 -0300

    Adiciona rotas de cadastro e lista de cadastrados mockados

--- app.py
+++ app.py
@@ -11,14 +11,23 @@ app = Flask('__name__')
 app.config["SQLALCHEMY_DATABASE_URI"] = f'mysql+pymysql://{config.DB_USER}:{config.DB_PWD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}'
 db = SQLAlchemy(app)
 
-with app.app_context(): #equivalente a @app.route('/') no projeto do teste_db
-
+@app.route('/prestadores')
+def cadastrados():
     #Select que retorna algumas informações mais relevantes sobre o candidato
     result1 = db.session.query(Candidato, Contratacao, InformacoesExtras).join(Contratacao).join(InformacoesExtras).all()
-    for i in result1: #0 se refere a candidato, 1 a contratação e 2 a infoormações extras
-        print(i[0].p_nome, i[0].cidade, i[0].telefone, i[0].exp_anos, i[1].tipo_contrato, i[2].certificacao)
+    for i in result1: #0 se refere a candidato, 1 a contratação e 2 a informações extras
+        print(i[0].p_nome+' '+i[0].sobrenome, i[0].cidade, i[0].telefone, i[0].exp_anos, i[0].hora_extra, i[1].tipo_contrato, i[2].certificacao)
 
     #Select que retorna o nome do candidato e as suas habilidades
-    result2 = db.session.query(HabilidadesCandidato, Candidato, Habilidade).join(Candidato).join(Habilidade).all()
-    for i in result2:
-        print(i[1].p_nome, i[2].hab_nome)
+    #result2 = db.session.query(HabilidadesCandidato, Candidato, Habilidade).join(Candidato).join(Habilidade).all()
+    #for i in result2:
+        #print(i[1].p_nome, i[2].hab_nome)
+
+    return render_template('admin.html', cadastrobasico = result1)
+
+@app.route('/')
+def cadastro():
+    return render_template('trabalhe.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
